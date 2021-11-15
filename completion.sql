2b9004e799d4038873601eee14f79179f8251cb0
Denis <45011033+feezeee@users.noreply.github.com>
2021-11-15 21:54:40 +0300

    ready

--- completion.sql
+++ completion.sql
@@ -2,7 +2,6 @@ use school;
 
 insert into access_rights (access_right_type) values
 ("Администратор"),
-("Ученик"),
 ("Учитель");
 
 
@@ -17,10 +16,10 @@ insert into qualifications (qualification_name) values
 ("Историк");
 
 insert into workers (last_name, first_name, middle_name, phone_number, email, password, position_id, qualification_id, access_right_id) values
-("Сиваченко", "Юлия", "Сергеевна", "+375291234567", "йцукенгшщзхъ", "root", 1, 1, 1);
+("Сиваченко", "Юлия", "Сергеевна", "+375291234567", "test1@mail.com", "1234", 1, 1, 1);
 
 insert into workers (last_name, first_name, middle_name, phone_number, email, password, position_id, qualification_id, access_right_id) values
-("Кириленко", "Наталья", "Геннадьевна", "+375297654321", "йцукенгшщзхъ", "root", 3, 1, 1);
+("Кириленко", "Наталья", "Геннадьевна", "+375297654321", "test2@mail.com", "1234", 3, 1, 1);
 
 insert into subjects (subject_name, worker_id) values
 ("Математика", 2),
@@ -30,7 +29,13 @@ insert into subjects (subject_name, worker_id) values
 insert into classes_char (char_name) values
 ("А"),
 ("Б"),
-("В");
+("В"),
+("Г"),
+("Д"),
+("Е"),
+("Ж"),
+("З"),
+("И");
 
 insert into classes_type (type_number) values
 (1),
@@ -51,11 +56,11 @@ insert into classes (class_type_id, classroom_teacher_worker_id, class_char_id )
 (2, 1, 2);
 
 insert into students (last_name, first_name, middle_name, class_id) values
-("Ученик1ф", "Ученик1и", "Ученик1о", 1),
-("Ученик2ф", "Ученик2и", "Ученик2о", 2);
+("Иванов", "Иван", "Иванович", 1),
+("Александров", "Александр", "Александрович", 2);
 
 
 insert into schedules (date, class_id, subject_id) values
-("2021-11-12 01:05:47", 1, 1),
-("2021-11-11 01:05:47", 1, 2),
-("2021-11-9 01:05:47", 1, 3);
+("2021-11-12 09:00:00", 1, 1),
+("2021-11-11 09:00:00", 1, 2),
+("2021-11-9 09:00:00", 1, 3);
