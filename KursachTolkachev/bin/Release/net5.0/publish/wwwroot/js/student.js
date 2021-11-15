99b557f54c1e6e1b97ccdbebfef6a09914972936
Denis <45011033+feezeee@users.noreply.github.com>
2021-11-15 22:01:08 +0300

    publish

---
+++ KursachTolkachev/bin/Release/net5.0/publish/wwwroot/js/student.js
@@ -0,0 +1,8 @@
+﻿
+    $(function () {
+        $('#editStudent').on('click', '.rowEditStart', function () {
+            var id = parseInt($(this).find('.editId').html());
+            document.location.href = '/Student/Edit?id=' + id;
+            //alert($(this).find('.editId').html());
+        });
+    });
---
+++ KursachTolkachev/bin/Release/net5.0/publish/wwwroot/js/worker.js
@@ -0,0 +1,9 @@
+﻿
+    $(function () {
+        $('#editWorker').on('click', '.rowEditStart', function () {
+            var id = parseInt($(this).find('.editId').html());
+            document.location.href = '/Worker/Edit?id=' + id;
+            //alert($(this).find('.editId').html());
+        });
+    });
+
