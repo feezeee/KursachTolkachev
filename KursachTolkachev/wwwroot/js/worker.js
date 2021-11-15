d7ae9fd58c3b7bbd990664eeac66c5a7ec67a20c
Denis <45011033+feezeee@users.noreply.github.com>
2021-11-15 21:25:24 +0300

    test

--- KursachTolkachev/wwwroot/js/worker.js
+++ KursachTolkachev/wwwroot/js/worker.js
@@ -1,9 +1,7 @@
-﻿if (User.IsInRole("Администратор") || User.IsInRole("Директор")) {
-    $(function () {
-        $('#editWorker').on('click', '.rowEditStart', function () {
-            var id = parseInt($(this).find('.editId').html());
-            document.location.href = '/Worker/Edit?id=' + id;
-            //alert($(this).find('.editId').html());
-        });
+﻿$(function () {
+    $('#editWorker').on('click', '.rowEditStart', function () {
+        var id = parseInt($(this).find('.editId').html());
+        document.location.href = '/Worker/Edit?id=' + id;
+        //alert($(this).find('.editId').html());
     });
-}
+});
